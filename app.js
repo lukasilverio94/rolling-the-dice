60c2789afe2fed2461bc08aa0337b7d809de1a5a
Lucas Silverio <117397454+lukasilverio94@users.noreply.github.com>
2023-04-10 01:46:59 +0200

    Almost done

---
+++ app.js
@@ -0,0 +1,28 @@
+//dice 1
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+let randomDiceImg = "dice" + randomNumber1 + ".png"; // dice2.png
+
+let randomImgSrc = "img/" + randomDiceImg; // img/dice2.png
+
+let image1 = document.querySelectorAll("img")[0]; //capturar img (com query selector all)
+
+image1.setAttribute("src", randomImgSrc);
+
+//dice 2
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let randomImgSrc2 = "img/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImgSrc2);
+
+//condicional resultado do jogo
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🏆Player 1 won!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "🏆Player 2 won!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw! Try again!";
+}
