7da08c13e8a5b15188e9b9fd80c9548d9a4d230c
Lucas Silverio <117397454+lukasilverio94@users.noreply.github.com>
2023-04-10 22:45:29 +0200

    almost finish project
    still need to solve the problem of the image that isnt loading when i open the browser.

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
 //dice 1
-let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let rollButton = document.getElementById("rollButton");
 
-let randomDiceImg = "dice" + randomNumber1 + ".png"; // dice2.png
+rollButton.addEventListener("click", function () {
+  let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  let randomImgSrc = "img/dice" + randomNumber1 + ".png";
+  let image1 = document
+    .querySelectorAll("img")[0]
+    .setAttribute("src", randomImgSrc); //capturar img
+  let randomImgSrc2 = "img/dice" + randomNumber2 + ".png";
+  let image2 = document
+    .querySelectorAll("img")[1]
+    .setAttribute("src", randomImgSrc2);
+  let title = document.querySelector("h1");
 
-let randomImgSrc = "img/" + randomDiceImg; // img/dice2.png
+  //condicional resultado do jogo
 
-let image1 = document.querySelectorAll("img")[0]; //capturar img (com query selector all)
-
-image1.setAttribute("src", randomImgSrc);
-
-//dice 2
-
-let randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-let randomImgSrc2 = "img/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImgSrc2);
-
-//condicional resultado do jogo
-
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🏆Player 1 won!";
-} else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "🏆Player 2 won!";
-} else {
-  document.querySelector("h1").innerHTML = "Draw! Try again!";
-}
+  if (randomNumber1 > randomNumber2) {
+    title.innerHTML = "🏆Player 1 won!";
+  } else if (randomNumber2 > randomNumber1) {
+    title.innerHTML = "🏆Player 2 won!";
+  } else {
+    title.innerHTML = "Draw! Try again!";
+  }
+});
